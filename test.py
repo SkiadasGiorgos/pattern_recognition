275a68f880272448bdf1ffc18bb477b2a77cd7a4
Konstantiss <konstantinos.michopoulos@gmail.com>
2022-11-03 16:54:04 +0200

    Update probability_distribution().

--- test.py
+++ test.py
@@ -10,18 +10,18 @@ p_omega_1 = 0.95
 p_omega_2 = 0.05
 
 sigma = np.array([[1.5, 0], [0, .8]])
-# x_0 = np.arange(-5,6,1)
-# x_1 = np.arange(-5,6,1)
+# x_0 = np.random.normal(1, 2, 10)
+# x_1 = np.random.normal(1.2, 1.8, 10)
 x_0 = np.arange(-5, 5, 1)
 x_1 = np.arange(-5, 5, 1)
 x = np.array([x_0, x_1]).transpose()
+d = x.shape[1]
 print(x.shape[0])
 x_0, x_1 = np.meshgrid(x_0, x_1)
 
 # Answer to question 1
-def distribution_value(x, mu, sigma):
-    d = mu.shape[0]
-    if not (isinstance(x, (int, np.integer))):
+def distribution_values(x, mu, sigma):
+    if not (isinstance(x, (int, np.integer, float))):
         p = np.zeros(x.shape[0])
         for i in range(x.shape[0] - 1):
             p[i] = 1 / (pow((2 * np.pi), (d / 2)) * np.sqrt(np.linalg.det(sigma))) * np.exp(
@@ -32,27 +32,24 @@ def distribution_value(x, mu, sigma):
     return p
 
 
-print(distribution_value(x, mu_1, sigma))
-print(distribution_value(x, mu_2, sigma))
+print(distribution_values(x, mu_1, sigma))
+print(distribution_values(x, mu_2, sigma))
 
 
 # Answer to question 2
-def probability_distribution(x, mu_1, mu_2, sigma):
-    d = mu_1.shape[0]
+def probability_distribution(distribution_values_1, distribution_values_2):
 
     probability_distrib = np.array([])
 
-    for i in range(x.shape[0] - 1):
-        for j in range(x.shape[1] - 1):
-            probability_distrib = np.append(probability_distrib,
-                                            distribution_value(x[i, j], mu_1, sigma) * p_omega_1 + distribution_value(
-                                                x[i, j], mu_2,
-                                                sigma) * p_omega_2)
+    for i in range(distribution_values_1.shape[0]):
+        probability_distrib = np.append(probability_distrib, distribution_values_1[i] * p_omega_1 + distribution_values_2[i] * p_omega_2)
 
     return probability_distrib
 
 
-print(probability_distribution(x, mu_1, mu_2, sigma))
+distribution_values_1 = distribution_values(x, mu_1, sigma)
+distribution_values_2 = distribution_values(x, mu_2, sigma)
+print(probability_distribution(distribution_values_1, distribution_values_2))
 
 
 def discriminant_function(x_0, x_1, mu, sigma, a_priori):
@@ -81,7 +78,7 @@ ax = plt.axes(projection='3d')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
-# plt.show()
+plt.show()
 
 
 # def bayes_error(p_omega_1, p_omega_2, mu_1, mu_2, sigma):
