b111358ae422869fa0c0d3cfaddb2496a63fa92f
Konstantiss <konstantinos.michopoulos@gmail.com>
2022-11-04 17:37:19 +0200

    Change first plot.

--- test.py
+++ test.py
@@ -12,28 +12,34 @@ p_omega_2 = 0.05
 sigma = np.array([[1.5, 0], [0, .8]])
 # x_0 = np.random.normal(1, 2, 10)
 # x_1 = np.random.normal(1.2, 1.8, 10)
-x_0 = np.arange(-5, 5, 1)
-x_1 = np.arange(-5, 5, 1)
-x = np.array([x_0, x_1]).transpose()
-d = x.shape[1]
-print(x.shape[0])
+x_0 = np.arange(-10, 10, .1)
+x_1 = np.arange(-10, 10, .1)
+d = 2
 x_0, x_1 = np.meshgrid(x_0, x_1)
+x = np.array([x_0, x_1]).transpose()
 
 # Answer to question 1
 def distribution_values(x, mu, sigma):
     if not (isinstance(x, (int, np.integer, float))):
-        p = np.zeros(x.shape[0])
-        for i in range(x.shape[0] - 1):
-            p[i] = 1 / (pow((2 * np.pi), (d / 2)) * np.sqrt(np.linalg.det(sigma))) * np.exp(
-                -0.5 * np.matmul(np.matmul(np.transpose(x[i, :] - mu), np.linalg.inv(sigma)), (x[i, :] - mu)))
+        p = np.zeros([x.shape[0], x.shape[1]])
+        for i in range(x.shape[0]):
+            for j in range(x.shape[1]):
+                p[i,j] = 1 / (pow((2 * np.pi), (d / 2)) * np.sqrt(np.linalg.det(sigma))) * np.exp(
+                    -0.5 * np.matmul(np.matmul(np.transpose(x[i, j] - mu), np.linalg.inv(sigma)), (x[i, j] - mu)))
     else:
         p = 1 / (pow((2 * np.pi), (d / 2)) * np.sqrt(np.linalg.det(sigma))) * np.exp(
             -0.5 * np.matmul(np.matmul(np.transpose(x - mu), np.linalg.inv(sigma)), (x - mu)))
     return p
 
+fig1, ax1 = plt.subplots(subplot_kw={"projection": "3d"})
+distribution_values_1 = distribution_values(x, mu_1, sigma)
+distribution_values_2 = distribution_values(x, mu_2, sigma)
+fig = plt.figure()
+fig.add_subplot()
+surface_mu_1= ax1.plot_surface(x_0, x_1, distribution_values_1)
+surface_mu_2= ax1.plot_surface(x_0, x_1, distribution_values_2)
+plt.show()
 
-print(distribution_values(x, mu_1, sigma))
-print(distribution_values(x, mu_2, sigma))
 
 
 # Answer to question 2
@@ -78,7 +84,7 @@ ax = plt.axes(projection='3d')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
-plt.show()
+#plt.show()
 
 
 # def bayes_error(p_omega_1, p_omega_2, mu_1, mu_2, sigma):
