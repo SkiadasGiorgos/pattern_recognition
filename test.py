88dd4fc447b0837ecfca1416c3b399f73659c2e2
Skiadas Giorgos <skiadas.giorgos04@gmail.com>
2022-11-04 13:40:24 +0200

    Add plots for first question

--- test.py
+++ test.py
@@ -10,25 +10,32 @@ p_omega_2 = 0.05
 
 sigma = np.array([[1.5, 0], [0, .8]])
 x = np.array([1, 0])
-x_0 = np.arange(-1, 1, .1)
-x_1 = np.arange(-1, 1, .1)
+x_0 = np.arange(-10, 10, .1)
+x_1 = np.arange(-10, 10, .1)
 x_0, x_1 = np.meshgrid(x_0, x_1)
 
 
-def distribution_prices(x, mu, sigma):
+def distribution_prices(x_0,x_1, mu, sigma):
     d = mu.shape[0]
-    p = np.zeros(x.shape[0])
-    p = 1 / (pow((2 * np.pi), (d / 2)) * np.sqrt(np.linalg.det(sigma))) * np.exp(
-        -0.5 * np.matmul(np.matmul(np.transpose(x - mu), np.linalg.inv(sigma)), (x - mu)))
-    # for i in range(x.shape[0]):
-    #     p[i] = 1 / (pow((2 * np.pi), (d / 2)) * np.sqrt(np.linalg.det(sigma))) * np.exp(
-    #         -0.5 * np.matmul(np.matmul(x[:, i] - mu, np.transpose(sigma)), (x[:, i] - mu)))
+    p = np.zeros([x_0.shape[0], x_0.shape[1]])
+    # p = 1 / (pow((2 * np.pi), (d / 2)) * np.sqrt(np.linalg.det(sigma))) * np.exp(
+    #     -0.5 * np.matmul(np.matmul(np.transpose(x - mu), np.linalg.inv(sigma)), (x - mu)))
+    for i in range(x_0.shape[0]):
+        for j in range(x_1.shape[0]):
+            p[i][j] = 1 / (pow((2 * np.pi), (d / 2)) * np.sqrt(np.linalg.det(sigma))) * np.exp(
+                -0.5 * np.matmul(np.matmul(np.transpose(np.subtract([x_0[i][j], x_1[i][j]] ,mu)), np.linalg.inv(sigma)), np.subtract([x_0[i][j], x_1[i][j]] ,mu)))
     return p
 
 
-# print(distribution_prices(x, mu_1, sigma))
-
-
+# print(distribution_prices(x_0,x_1,mu_1, sigma))
+fig1, ax1 = plt.subplots(subplot_kw={"projection": "3d"})
+distrib_prices_1 = distribution_prices(x_0,x_1,mu_1,sigma)
+distrib_prices_2 = distribution_prices(x_0,x_1,mu_2,sigma)
+fig = plt.figure()
+fig.add_subplot()
+surface_mu_1= ax1.plot_surface(x_0, x_1, distrib_prices_1)
+surface_mu_2= ax1.plot_surface(x_0, x_1, distrib_prices_2)
+plt.show()
 def probability_distribution(x, mu_1, mu_2, sigma_1, sigma_2):
     d = mu_1.shape[0]
 
@@ -65,19 +72,19 @@ def discriminant_function(x_0, x_1, mu, sigma, a_priori):
 
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
-x = discriminant_function(x_0, x_1, mu_2, sigma, p_omega_2)
-y = discriminant_function(x_0, x_1, mu_1, sigma, p_omega_1)
-
-fig = plt.figure()
-fig.add_subplot()
-surf = ax.plot_surface(x_0, x_1, x)
-surf = ax.plot_surface(x_0, x_1, y)
-
-ax = plt.axes(projection='3d')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-plt.show()
+# x = discriminant_function(x_0, x_1, mu_2, sigma, p_omega_2)
+# y = discriminant_function(x_0, x_1, mu_1, sigma, p_omega_1)
+#
+# fig = plt.figure()
+# fig.add_subplot()
+# surf = ax.plot_surface(x_0, x_1, x)
+# surf = ax.plot_surface(x_0, x_1, y)
+#
+# ax = plt.axes(projection='3d')
+# ax.set_xlabel('x')
+# ax.set_ylabel('y')
+# ax.set_zlabel('z')
+# plt.show()
 
 
 def bayes_error(p_omega_1, p_omega_2, mu_1, mu_2, sigma):
